a1c90ae5c8a517eb072e56b2aed5b29aa0719dda
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2023-09-21 15:37:05 +0700

    lastest

--- db.js
+++
@@ -1,3 +0,0 @@
-const pgp = require('pg-promise')();
-const db = pgp(process.env.DATABASE_URL || 'postgresql://webadmin:VTTboo64357@node52113-project-advcompro.proen.app.ruk-com.cloud:11349/postgres');
-module.exports = db;
